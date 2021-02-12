ff9c9c744087469e1d9589cc2f7b7e4329794c9c
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-02-12 21:07:28 +0530

    #1464 Max products of elements

---
+++ src/main/java/com/leetcode/miscellaneous/arrays/MaxProductOfTwoElements.java
@@ -0,0 +1,17 @@
+package com.leetcode.miscellaneous.arrays;
+
+import java.util.Arrays;
+
+/**
+ * #1464 https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ */
+public class MaxProductOfTwoElements {
+    public static void main(String[] args) {
+        int []arr = {1,5,4,5,1,2,3,4,5,6,7,8,9,56,43,2,212,34,3};
+        System.out.println(maxProduct(arr));
+    }
+    static int maxProduct(int []arr){
+        Arrays.sort(arr);
+        return ((arr[arr.length-1]-1) * (arr[arr.length-2]-1));
+    }
+}
