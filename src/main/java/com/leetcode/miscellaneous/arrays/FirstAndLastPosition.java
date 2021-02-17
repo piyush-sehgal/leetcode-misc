5348a055e049e37d8e03c14850d2f18166dfd9a2
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-02-17 14:16:24 +0530

    #34 First and last position of an element

---
+++ src/main/java/com/leetcode/miscellaneous/arrays/FirstAndLastPosition.java
@@ -0,0 +1,36 @@
+package com.leetcode.miscellaneous.arrays;
+
+/**
+ * #34 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ */
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int []arr = {1};
+        //{1,1,2,2,2,2,3,4,5,5,6};
+        int target = 1;
+        int []result = searchRange(arr, target);
+        for(int i = 0; i < result.length; i++){
+            System.out.println(result[i]);
+        }
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        /**
+         *  TC: O(n)
+         *  Using linear search
+         */
+        int []result = {-1,-1};
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                result[0] = i;
+                break;
+            }
+        }
+        for(int i = nums.length-1; i >= 0; i--){
+            if(nums[i] == target){
+                result[1] = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
