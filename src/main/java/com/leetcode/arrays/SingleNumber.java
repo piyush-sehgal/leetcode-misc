d507df1f18a76ef8fe1a0fcc82f8bf8cf87fa66a
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-02-27 23:04:53 +0530

    #136 single number / set approach /  TC : O(n) SC: O(n)

--- src/main/java/com/leetcode/arrays/SingleNumber.java
+++ src/main/java/com/leetcode/arrays/SingleNumber.java
@@ -1,7 +1,9 @@
 package com.leetcode.arrays;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * #136 https://leetcode.com/problems/single-number/
@@ -10,6 +12,7 @@ public class SingleNumber {
     public static void main(String[] args) {
         int []arr = {5,4,3,5,4,3,1};
         System.out.println(singleNumber(arr));
+        System.out.println(singleNumberOptimizedApproach(arr));
     }
     public static int singleNumber(int []arr){
         /**
@@ -27,5 +30,28 @@ public class SingleNumber {
             }
         }
         return singleList.get(0);
+    }
+
+    public static int singleNumberOptimizedApproach(int []arr){
+        /**
+         * Using set
+         * 2 ∗ (a+b+c) − (a+a+b+b+c) = c
+         * 2 * sumOfSet - sumOfNums
+         *
+         * TC: set.contains() -> O(1)
+         *     for -> O(n)
+         *    O(n) * O(1) = O(n)
+         * SC: O(n)
+         */
+        Set<Integer> set = new HashSet<Integer>();
+        int sumOfSet = 0, sumOfNums = 0;
+        for(int i: arr){
+            if(!set.contains(i)){
+                set.add(i);
+                sumOfSet += i;
+            }
+            sumOfNums += i;
+        }
+        return 2 * sumOfSet - sumOfNums;
     }
 }
