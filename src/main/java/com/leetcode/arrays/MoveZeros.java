a8c87ea883bf02165a453c7d2364a07ea7c2e4a2
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-02-27 17:08:18 +0530

    #283 Moving zeros(reduced space complexity)

--- src/main/java/com/leetcode/arrays/MoveZeros.java
+++ src/main/java/com/leetcode/arrays/MoveZeros.java
@@ -7,6 +7,7 @@ public class MoveZeros {
     public static void main(String[] args) {
         int []arr = {0,1,0,3,12};
         moveZeros(arr);
+        moveZerosNoExtraSpace();
     }
     public static void moveZeros(int []arr){
         /**
@@ -25,6 +26,26 @@ public class MoveZeros {
         }
         for(int i = 0; i < size; i++){
             arr[i] = copy[i];
+        }
+    }
+
+    public static void moveZerosNoExtraSpace(int []arr){
+        /**
+         * TC: O(n)
+         * SC: O(1)
+         */
+        int i = 0;
+        for(int j = 1; j < arr.length; j++){
+            if(arr[i] == 0){
+                if(arr[j] != 0){
+                    arr[i] = arr[j];
+                    arr[j] = 0;
+                    i++;
+                }
+            }
+            else{
+                i++;
+            }
         }
     }
 }
