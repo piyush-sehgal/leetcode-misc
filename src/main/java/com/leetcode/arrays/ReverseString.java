73adf5bed9a0c778065da8925ea01cbb15f5f233
Piyush Sehgal <piyush97sehgal@gmail.com>
2021-02-28 12:33:37 +0530

    #344 reverse string / two pointer / TC: O(n) SC: O(1)

--- src/main/java/com/leetcode/arrays/ReverseString.java
+++ src/main/java/com/leetcode/arrays/ReverseString.java
@@ -6,9 +6,15 @@ package com.leetcode.arrays;
 public class ReverseString {
     public static void main(String[] args) {
         char[] str = {'h','e','l','l','o'};
+        char[] str2 = {'h','e','l','l','o'};
         reverseString(str);
         for(int i = 0; i < str.length; i++){
             System.out.print(str[i]+ " ");
+        }
+        System.out.println();
+        revereStringInPlace(str2);
+        for(int i = 0; i < str2.length; i++){
+            System.out.print(str2[i]+ " ");
         }
     }
     public static void reverseString(char []str){
@@ -23,6 +29,20 @@ public class ReverseString {
         }
         for(int i = 0; i < str.length; i++){
             str[i] = result[i];
+        }
+    }
+    public static void revereStringInPlace(char []str){
+        /**
+         * TC: O(n)
+         * SC: O(1)
+         */
+        int start = 0, end = str.length-1;
+        while(start < end){
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
         }
     }
 }
